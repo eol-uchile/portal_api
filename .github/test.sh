3c942d0c901a1eac531990f127fb55a20a15e988
fsologureng <fsologur@uchile.cl>
2024-07-31 10:10:51 -0400

    ci: centralize external workflow

---
+++ .github/test.sh
@@ -0,0 +1,15 @@
+#!/bin/dash
+
+set -e
+
+pip install -e /openedx/requirements/app
+
+cd /openedx/requirements/app
+cp /openedx/edx-platform/setup.cfg .
+mkdir test_root
+cd test_root/
+ln -s /openedx/staticfiles .
+
+cd /openedx/requirements/app
+
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest portal_api/tests/tests_lms.py
